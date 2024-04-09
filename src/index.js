345e2f6f594bcd861f3e48128df202458180f0bd
lnbEli <137424458+lnbEli@users.noreply.github.com>
2024-04-09 11:40:14 +0200

    Complete homepage layout with flex containers. Add copy for about section. Layout header buttons and logo.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import "./style.css";
+
+// const element = document.createElement("button");
+// const nav = document.querySelector("nav");
+// element.classList.add(".header");
+// nav.appendChild(element);
+
 console.log("Looks like it works......");
+console.log("wooooooooooooooowwwwwww");
+console.log("still");
